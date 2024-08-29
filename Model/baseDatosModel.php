ffe2b733f86aeb2a084157d99a99f3d3bd9bf875
Julian Madrigal <julianmadrigal1200@gmail.com>
2024-08-28 22:35:28 -0600

    Se crearon los models completos del proyecto

---
+++ Model/baseDatosModel.php
@@ -0,0 +1,27 @@
+<?php
+
+function AbrirOrcl()
+{
+    $username = 'Administrador';
+    $password = 'Administrador';
+    $dbname = 'XE';
+    $hostname = 'localhost';
+    $connString = "(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=$hostname)(PORT=1521))(CONNECT_DATA=(SERVICE_NAME=$dbname)))";
+    $conexion = oci_connect($username, $password, $connString, 'AL32UTF8');
+    if (!$conexion) {
+        $error = oci_error();
+        echo "Error de conexión: " . $error['message'];
+        return false;
+    }
+    return $conexion;
+}
+
+function CerrarOrcl($conexion)
+{
+    if ($conexion) {
+        oci_close($conexion);
+    }
+}
+
+?>
+
